373ca0d60bf68882733a02c6185a4180b2d001e1
WaronLimsakul <poonro@gmail.com>
2025-02-07 19:00:44 -0500

    Add command runner

--- main.go
+++ main.go
@@ -2,22 +2,78 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/WaronLimsakul/gator/internal/config"
 )
 
-func main() {
-	currentConfig, err := config.ReadConfig()
-	if err != nil {
-		fmt.Printf("%v", err)
+type state struct {
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("the login handler expects a single argument, the username")
+	}
+
+	loginName := cmd.args[0]
+	if err := s.config.SetUser(loginName); err != nil {
+		return err
+	}
+	fmt.Println("user has been set")
+	return nil
+}
+
+type commands struct {
+	cmdMap map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	if len(name) == 0 || f == nil {
 		return
 	}
-	currentConfig.SetUser("Ron")
-	currentConfig, err = config.ReadConfig()
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-	fmt.Printf("%v", currentConfig)
+	c.cmdMap[name] = f
 	return
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.cmdMap[cmd.name]
+	if !ok {
+		return fmt.Errorf("'%s' not found", cmd.name)
+	}
+
+	if err := handler(s, cmd); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	conf, err := config.ReadConfig()
+	if err != nil {
+		return
+	}
+	curState := state{config: &conf}
+
+	gatorCommands := commands{}
+	gatorCommands.cmdMap = make(map[string]func(*state, command) error)
+	gatorCommands.register("login", handlerLogin)
+
+	// os.Args first element is the location where these are store (/tmp/something)
+	if len(os.Args) < 2 {
+		fmt.Println("command required")
+		os.Exit(1)
+	}
+	inputCmd := command{os.Args[1], os.Args[2:]}
+	err = gatorCommands.run(&curState, inputCmd)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
